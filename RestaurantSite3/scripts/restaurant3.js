946407540c13627c7f65a582ca96491e32fb385f
Angela Kluenker <alkluenker@hotmail.com>
2020-02-27 17:33:16 -0600

    restaurant site form validation

--- RestaurantSite3/scripts/restaurant3.js
+++ RestaurantSite3/scripts/restaurant3.js
@@ -1,28 +1,133 @@
+document.getElementById("nameerror").style.display = "none";
 
+document.getElementById("emailerror").style.display = "none";
 
+document.getElementById("phoneerror").style.display = "none";
 
-function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
-    if (x == "") {
-      alert("First Name must be filled out");
-      return false;
-    }
+document.getElementById("infoerror").style.display = "none";
 
-    var y = document.forms["myForm"]["lname"].value;
-    if (y == "") {
-      alert("Last Name must be filled out");
-      return false;
-    }
-  }
+document.getElementById("dayserror").style.display = "none";
 
-  function validateForm2() {
+document.getElementById("validinfo").style.display = "none";
 
-    var a = document.forms["contactform"]["name"].value;
-    if (a == "") {
-        alert("name must be filled out");
-        return false;
+function validateForm2() {
+
+  var a = document.forms["contactform"]["name"].value;
+  if (a == "") {
+        //alert("name must be filled out");
+        document.getElementById("nameerror").style.display = "inline";
         
-    }
+      return false;
+        
+  }
+
+  if (a !== "") {
+
+    document.getElementById("nameerror").style.display = "none";
 
   }
+
+
+  var b = document.forms["contactform"]["email"].value;
+
+  if (b == "") {
+      
+    document.getElementById("emailerror").style.display = "inline";
+      
+    return false;
+      
+  }
+
+  if (b !== "") {
+
+    document.getElementById("emailerror").style.display = "none";
+
+  }
+
+  var c = document.forms["contactform"]["phone"].value;
+
+  if (c == "") {
+      
+    document.getElementById("phoneerror").style.display = "inline";
+      
+    return false;
+      
+  }
+
+  if (c !== "") {
+
+    document.getElementById("phoneerror").style.display = "none";
+
+  }
+
+  
+
+  var d = document.forms["contactform"]["info"].value;
+
+  if (d == "") {
+      
+    document.getElementById("infoerror").style.display = "inline";
+      
+    return false;
+      
+  }
+
+  if (d !== "") {
+
+    document.getElementById("infoerror").style.display = "none";
+
+  }
+
+ 
+
+  if(validateChecks()==false) {
+    
+    document.getElementById("dayserror").style.display = "inline";
+
+    return false;
+  }
+ 
+  
+
+  
+
+
+}
+
+
+function validateChecks() {
+  var chks = document.getElementsByName("options[]");
+  var checkCount = 0;
+  for (var i = 0; i < chks.length; i++) {
+    if (chks[i].checked) {
+      checkCount++;
+    }
+  }
+  if (checkCount < 1) {
+    return false;
+  }
+
+  else {
+    
+    alert("All information is valid. Thank you.")
+
+    document.getElementById("validinfo").style.display = "inline";
+
+  }
+  
+}
+	
+  
+
+  
+  
+  
+
+
+
+
+
+
+  
+        
 
